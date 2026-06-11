38c60b17a2c0c8fba1b2934607d8589f439ee7ca
Karim Mahmoud <128514567+Karimgebril09@users.noreply.github.com>
2026-06-11 14:25:44 +0300

    add: inference engine with strategies

--- defenders/pii_detection/src/strategies.py
+++ defenders/pii_detection/src/strategies.py
@@ -6,19 +6,19 @@ class PIIStrategy:
 
 
 class MaskStrategy(PIIStrategy):
-    def apply(self, words, tags):
-        return " ".join(f"<{t[2:]}>" if t != "O" else w for w, t in zip(words, tags))
+    def apply(self, word_tags_pairs):
+        return " ".join(f"<{t[2:]}>" if t != "O" else w for w, t in word_tags_pairs)
 
 class EncryptStrategy(PIIStrategy):
     def _enc(self, x):
         return hashlib.sha256(x.encode()).hexdigest()[:10]
 
-    def apply(self, words, tags):
-        return " ".join(self._enc(w) if t != "O" else w for w, t in zip(words, tags))
+    def apply(self, word_tags_pairs):
+        return " ".join(self._enc(w) if t != "O" else w for w, t in word_tags_pairs)
 
 
 class BlockStrategy(PIIStrategy):
-    def apply(self, words, tags):
-        if sum([t != "O" for t in tags]) > 0:
+    def apply(self, word_tags_pairs):
+        if sum([t != "O" for w, t in word_tags_pairs]) > 0:
             return "[BLOCKED: PII DETECTED]"
-        return " ".join(words)
+        return " ".join(w for w, t in word_tags_pairs)
