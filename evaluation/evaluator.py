685a9c7ac58aa84949a4c77cb3c0b3619eeb0e70
jpassica <jammalpassica@gmail.com>
2026-06-13 22:43:59 +0300

    revert evaluator edit

--- evaluation/evaluator.py
+++ evaluation/evaluator.py
@@ -1,6 +1,3 @@
-from pathlib import Path
-
-from defenders.obfuscation.stage8_custom_classifier import load_stage8_distilbert_classifier
 from evaluation.refusal.inference.refusal_predictor import RefusalPredictor
 from evaluation.harm_detection.llm_judge import SentinelJudge
 
@@ -9,19 +6,11 @@ class Evaluator:
         self.judge = SentinelJudge(model_type=type_judge,model_name=judge,harm_type="evaluation",\
                                    base_url=base_url_judge, api_key=api_key_judge)
         self.refusal_predictor=RefusalPredictor()
-        artifacts_dir = (
-            Path(__file__).resolve().parents[1]
-            / "defenders"
-            / "obfuscation"
-            / "stage8_custom_classifier"
-            / "distilbert_artifacts_e4"
-        )
-        self.fine_tuned_harm_classifier = load_stage8_distilbert_classifier(artifacts_dir)
 
     def evaluate_response(self, llm_response):
         refusal_prediction = self.refusal_predictor.predict(llm_response)
-        fine_tuned_judge_result = self.fine_tuned_harm_classifier(llm_response)
-        fine_tuned_judge_pred = not bool(fine_tuned_judge_result.get("is_safe", True))
+        # TODO : add fine-tuned harm model 
+        fine_tuned_judge_pred = None
         return refusal_prediction or fine_tuned_judge_pred
     
     def evaluate_response_with_feedback(self,user_prompt, llm_response):
