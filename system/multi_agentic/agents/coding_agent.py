5e38ed957efd8e722b3a096ec1c5a12b1d94ac76
jpassica <jammalpassica@gmail.com>
2026-06-20 16:08:23 +0300

    minor edits to coding_agent

--- system/multi_agentic/agents/coding_agent.py
+++ system/multi_agentic/agents/coding_agent.py
@@ -1,14 +1,14 @@
-from langgraph.graph import StateGraph , START, END
+from langgraph.graph import StateGraph, START, END
 from typing import Literal
 from langchain_ollama import ChatOllama
-from langchain.messages import HumanMessage, SystemMessage  
+from langchain.messages import HumanMessage, SystemMessage
 from langchain.tools import tool
 from langgraph.prebuilt import ToolNode
 from langgraph.graph.message import MessagesState
 import traceback
 import warnings
 warnings.filterwarnings("ignore")
-from defenders.tools.code.src.code_defender import CodeDefender
+
 
 @tool
 def execute_code(code: str) -> str:
@@ -28,19 +28,20 @@ def execute_code(code: str) -> str:
         return str(e)
     except Exception:
         return traceback.format_exc()
-    
+
+
 class CodingAgentState(MessagesState):
     problem_description: str
     code: str
-    tool_defender: bool = True
-    defender: CodeDefender = CodeDefender(deep_check=True)
+
 
 def create_llms(model_id="qwen3:4b-instruct"):
-    coding_llm =ChatOllama(model=model_id).bind_tools([execute_code])
+    coding_llm = ChatOllama(model=model_id).bind_tools([execute_code])
     feedback_llm = ChatOllama(model=model_id)
     return coding_llm, feedback_llm
 
 coding_llm, feedback_llm = create_llms()
+
 
 def coding_agent(state: CodingAgentState) -> CodingAgentState:
     messages = [
@@ -49,36 +50,36 @@ def coding_agent(state: CodingAgentState) -> CodingAgentState:
                 "You are a coding agent. "
                 "Write ONLY code. No explanations. "
                 "Use Python unless specified otherwise."
-                "generate functional, efficient, and well-structured code."\
+                "generate functional, efficient, and well-structured code."
                 "Make sure only code is the output dont add any thing around it"
                 "dont do prints or any thing else just the code"
                 "you should generate only one solution for the problem described below"
                 "you should not generate anything related to markdown or any other format just pure code"
                 "only code is generated no explanations no markdown no comments just code"
-                "also if there are any feedbacks given by feedback agent consider them and improve the code accordingly."\
+                "also if there are any feedbacks given by feedback agent consider them and improve the code accordingly."
                 "if not ignore the feedback part"
-                "you have the choice to either make a test functions and call the tool to test it or" \
-                "directly generate the final code without making test cases and calling the tool"\
-                "if you feel something suspicious about the code you generated exit"\
-                f"feedback:{state['messages'][-1].content if len(state['messages'])>0 else 'no feedbacks given yet'}"
+                "you have the choice to either make a test functions and call the tool to test it or"
+                "directly generate the final code without making test cases and calling the tool"
+                "if you feel something suspicious about the code you generated exit"
+                f"feedback:{state['messages'][-1].content if len(state['messages']) > 0 else 'no feedbacks given yet'}"
             )
         ),
         HumanMessage(
-            content=f"Problem description:\n{state['problem_description']} "\
+            content=f"Problem description:\n{state['problem_description']} "
         ),
     ]
     resp = coding_llm.invoke(messages)
-    content=None
     if type(resp.content) == str:
         content = resp.content
     else:
         content = resp.content[0].get("text", str(resp.content))
     return {
         "code": content.strip(),
-        "messages": [resp]
+        "messages": [resp],
     }
 
-def feedback_agent(state: CodingAgentState) -> CodingAgentState:
+
+def feedback_agent(state: CodingAgentState) -> dict:
     messages = [
         SystemMessage(
             content="You are a feedback agent. Analyze test results and improve explain how to improve the code."
@@ -100,72 +101,73 @@ def feedback_agent(state: CodingAgentState) -> CodingAgentState:
     resp = feedback_llm.invoke(messages)
 
     return {
-        **state,
         "code": resp.content.strip(),
         "messages": state["messages"] + [resp],
     }
 
-def router1(state: CodingAgentState) -> Literal["feedback", "end"]:
+
+def router1(state: CodingAgentState) -> Literal["cont", "end"]:
     if "ALL_TEST_CASES_PASSED" in state["messages"][-1].content:
         return "end"
     return "cont"
 
 
-def router2(state: CodingAgentState) -> Literal["execute Tool", "end"]:
-    last_msg = state["messages"][-1]
-    print(state)
-    if hasattr(last_msg, "tool_calls") and last_msg.tool_calls:
-        if state["tool_defender"] == False:
-            print("Defense is deactivated")
-            return "execute Tool"
+def build_coding_agent(code_execution_protection: bool = True):
+    if code_execution_protection:
+        from defenders.tools.code.src.code_defender import CodeDefender
+        defender = CodeDefender(deep_check=True)
 
-        code_to_be_executed = last_msg.tool_calls[0]["args"]["code"]
-        print("defense activated")
-        if state.get("tool_defender", True) and state["defender"].is_safe(code_to_be_executed):
-            print("Code is safe to execute")
-            return "execute Tool"
-    
-    return "end"
-    
-def build_coding_agent():
+        def router2(state: CodingAgentState) -> Literal["execute Tool", "end"]:
+            last_msg = state["messages"][-1]
+            tool_calls = getattr(last_msg, "tool_calls", None)
+            if tool_calls:
+                code_to_execute = tool_calls[0]["args"]["code"]
+                safe, reason = defender.is_safe(code_to_execute)
+                if safe:
+                    print(f"[CodeDefender] Code is safe: {reason}")
+                    return "execute Tool"
+                print(f"[CodeDefender] BLOCKED – {reason}")
+            return "end"
+    else:
+        def router2(state: CodingAgentState) -> Literal["execute Tool", "end"]:
+            last_msg = state["messages"][-1]
+            tool_calls = getattr(last_msg, "tool_calls", None)
+            if tool_calls:
+                return "execute Tool"
+            return "end"
+
     graph = StateGraph(CodingAgentState)
     graph.add_node("coding", coding_agent)
     graph.add_node("execute Tool", ToolNode([execute_code]))
     graph.add_node("feedback", feedback_agent)
+
     graph.add_edge(START, "coding")
     graph.add_edge("feedback", "coding")
+
     graph.add_conditional_edges(
         "execute Tool",
         router1,
-        {
-            "cont": "feedback",
-            "end": END,
-        },
+        {"cont": "feedback", "end": END},
     )
-
     graph.add_conditional_edges(
         "coding",
         router2,
-        {
-            "execute Tool": "execute Tool",
-            "end": END,
-        },
+        {"execute Tool": "execute Tool", "end": END},
     )
-    app = graph.compile()
-    return app
+
+    return graph.compile()
 
 
 if __name__ == "__main__":
-    app = build_coding_agent()
-    code_defender = CodeDefender(deep_check=True)
+    app = build_coding_agent(code_execution_protection=True)
     initial_state = {
-        "problem_description": "Write a function that takes a list of integers and "
-        "returns the sum of all even numbers in the list."
-        "you should make some tests and test them to make sure of the correctness of the code and make tool call to execute the tests and return the results of the tests"
-        "again make sure to make execution of the tool it is urgent (TOOL CALL IS MANDATORY) and return the results of the tests and if they fail improve the code accordingly",
+        "problem_description": (
+            "Write a function that takes a list of integers and "
+            "returns the sum of all even numbers in the list."
+            "you should make some tests and test them to make sure of the correctness of the code and make tool call to execute the tests and return the results of the tests"
+            "again make sure to make execution of the tool it is urgent (TOOL CALL IS MANDATORY) and return the results of the tests and if they fail improve the code accordingly"
+        ),
         "code": "",
-        "tool_defender": True,
-        "defender": code_defender,
         "messages": [],
     }
     final_state = app.invoke(initial_state)
