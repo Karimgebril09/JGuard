5638831bf47292844b3c2334d8937db37f91accd
Mariam Amin <128838373+Mariam-Amin12@users.noreply.github.com>
2026-06-22 13:42:14 +0300

    fix bug

--- evaluation/refusal/inference/predict_dl_bert.py
+++ evaluation/refusal/inference/predict_dl_bert.py
@@ -1,32 +1,61 @@
 
 
 
-from matplotlib.pyplot import clf
 import re
 import torch
-from  evaluation.refusal.inference.fasttext_refusal_model import remove_non_english_char
 from torch import nn
 from transformers import DistilBertTokenizerFast, DistilBertModel
+import re
+
+def clean_text(text):
+    text = str(text)
+    #remove URLs
+    text = re.sub(r'http\S+|www\S+', ' ', text)
+    #remove HTML
+    text = re.sub(r'<.*?>', ' ', text)
+    #remove  spaces/newlines/tabs
+    text = re.sub(r'\s+', ' ', text)
+    #remove strange characters
+    text = re.sub(r'[^\w\s.,!?;:\'\"()-]', ' ', text)
+    return text.strip()
+
+def split_text_inference(text, chunk_size=100):
+    """split text into chunks to fit model input size"""
+    words = text.split()
+    chunks = []
+    for i in range(0, len(words), chunk_size):
+        chunk = " ".join(words[i:i+chunk_size])
+        chunks.append(chunk)
+    return chunks 
 
 def get_contextualized_embeddings(sentence, model, tokenizer, max_len, device='cuda'):
-  
+    """ generate contextualized word embeddings for a given sentence """
+
     device = "cuda" if torch.cuda.is_available() else "cpu"
     all_sent_word_embeddings = []
     words = sentence.split()
-
-    encoded = tokenizer(words, return_tensors="pt",is_split_into_words=True,truncation=True,padding="longest")
+    encoded = tokenizer(
+        words,
+        return_tensors="pt",
+        is_split_into_words=True, #to keep mapping between words and subwords
+        truncation=True,
+        padding="longest"
+    )
+    
     for k in encoded:
-        encoded[k] = encoded[k].to(device)
+        encoded[k] = encoded[k].to(device) #move to gpu 
 
-    with torch.no_grad():
+    with torch.no_grad():  # inference only, no gradients needed
         outputs = model(**encoded)
-    hidden = outputs.last_hidden_state[0]
+
+    hidden = outputs.last_hidden_state[0]# extract the hidden states for the first each token
+
     word_ids = encoded.word_ids(batch_index=0)
 
     word_embs = []
-    for w_idx in range(len(words)):
+    for w_idx in range(len(words)): # find subtoken position 
         indices = [i for i, wid in enumerate(word_ids) if wid == w_idx]
-        if indices:
+        if indices:  # average subtoken 
             emb = hidden[indices].mean(dim=0)
         else:
             emb = torch.zeros(model.config.hidden_size, device=device)
@@ -34,38 +63,51 @@ def get_contextualized_embeddings(sentence, model, tokenizer, max_len, device='c
 
     if len(word_embs) < max_len:
         pad_len = max_len - len(word_embs)
-        pad = torch.zeros(model.config.hidden_size, device=device)
+        pad = torch.zeros(model.config.hidden_size, device=device) # pad with the same hidden size  
         word_embs += [pad] * pad_len
-    all_sent_word_embeddings.append(torch.stack(word_embs))
-    final_tensor = torch.stack(all_sent_word_embeddings)
+
+    all_sent_word_embeddings.append(torch.stack(word_embs)) #stack (sent_len, hidden_size)
+
+    final_tensor = torch.stack(all_sent_word_embeddings) #for batch dimension (1,sent_len, hidden_size)
     return final_tensor
 
 
 class LSTMClassifier(nn.Module):
-    def __init__(self, embed_dim=768, hidden_dim=256, dropout=0.3): 
+    def __init__(self, embed_dim=768, hidden_dim=256, dropout=0.3):
         super().__init__()
-        self.lstm = nn.LSTM(
-            embed_dim, 
-            hidden_dim, 
-            num_layers=2, 
-            batch_first=True, 
-            dropout=dropout if dropout > 0 else 0,
-            bidirectional=True 
-        )
-        
-        self.dropout = nn.Dropout(dropout)
-        self.relu = nn.ReLU() 
 
-        self.fc1 = nn.Linear(hidden_dim, hidden_dim)
+        self.lstm = nn.LSTM(
+            embed_dim,
+            hidden_dim,
+            num_layers=2,
+            batch_first=True,
+            dropout=dropout if dropout > 0 else 0,
+            bidirectional=True
+        )
+
+        self.dropout = nn.Dropout(dropout)
+        self.relu = nn.ReLU()
+
+        # bidirectional hidden_dim * 2
+        self.fc1 = nn.Linear(hidden_dim * 2, hidden_dim)
         self.fc2 = nn.Linear(hidden_dim, 1)
 
     def forward(self, x, lengths):
-        packed_input = nn.utils.rnn.pack_padded_sequence(
-            x, lengths.cpu(), batch_first=True, enforce_sorted=False
+
+        packed_input = nn.utils.rnn.pack_padded_sequence(  # to ignore padding that make lstm learn only from the real data
+            x,
+            lengths.cpu(),
+            batch_first=True,
+            enforce_sorted=False
         )
-        _, (h_n, _) = self.lstm(packed_input)
-        out = h_n[-1] 
-        
+
+        _, (h_n, _) = self.lstm(packed_input) 
+
+        forward = h_n[-2]
+        backward = h_n[-1]
+
+        out = torch.cat((forward, backward), dim=1)
+
         out = self.dropout(out)
         out = self.fc1(out)
         out = self.relu(out)
@@ -77,29 +119,38 @@ class LSTMClassifier(nn.Module):
 class RefusalInference:
     def __init__(self, embed_dim=768, hidden_dim=256, dropout=0.3):
         self.device = "cuda" if torch.cuda.is_available() else "cpu"
-        self.tokenizer=DistilBertTokenizerFast.from_pretrained(
-            "distilbert-base-uncased"
-        )
-        self.bert=DistilBertModel.from_pretrained(
-            "distilbert-base-uncased"
-        )
+        self.tokenizer=DistilBertTokenizerFast.from_pretrained(  "distilbert-base-uncased" )
+        self.bert=DistilBertModel.from_pretrained("distilbert-base-uncased" )
         self.bert.eval()
         self.model = LSTMClassifier(embed_dim, hidden_dim, dropout)
         self.model.load_state_dict(torch.load("./evaluation/refusal/models/lstm_refusal_model_bert.pt", map_location=self.device))
         self.model.to(self.device)
         self.model.eval()
-
+        
+    
     def predict(self, text):
-        cleaned_text=remove_non_english_char(text)
-        text = " ".join(cleaned_text.split()[:100])
-        emb = get_contextualized_embeddings(text, self.bert, self.tokenizer, max_len=len(text.split()), device=self.device)
-        length = torch.tensor([emb.shape[1]])
+        #apply clean 
+        cleaned_text = clean_text(text)
+        #split text so at the end take max prediction of all 
+        chunks = split_text_inference(cleaned_text, chunk_size=300)
+        scores = []
 
-        with torch.no_grad():
-            logit = self.model(emb.to(self.device), length.to(self.device))
-            prob = torch.sigmoid(logit).item()
+        for chunk in chunks:
+            
+            emb = get_contextualized_embeddings( chunk,self.bert, self.tokenizer, max_len=300, device=self.device )
+            length = torch.tensor([300])
+            with torch.no_grad():
+                logit = self.model(emb.to(self.device), length.to(self.device))
+                prob = torch.sigmoid(logit).item()
 
-        return {"label": int(prob >= 0.5), "score": prob}
+            scores.append(prob)
+
+        
+        final_score = max(scores)  # take the maximum score across all chunks 
+        return {
+            "label": int(final_score >= 0.5),
+            "score": final_score
+        }
     
 
 if __name__ == "__main__":
